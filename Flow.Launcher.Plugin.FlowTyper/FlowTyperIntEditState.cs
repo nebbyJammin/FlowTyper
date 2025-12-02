0349eaf3725c9cb33db3008fd062c970a23e5e4f
Benjamin Nguyen <bennguyen8180@gmail.com>
2025-12-03 04:27:43 +1100

    Cleaned up unused code

--- Flow.Launcher.Plugin.FlowTyper/FlowTyperIntEditState.cs
+++ Flow.Launcher.Plugin.FlowTyper/FlowTyperIntEditState.cs
@@ -12,31 +12,8 @@ namespace Flow.Launcher.Plugin.FlowTyper {
         IntField currIntField = IntField.NONE;
         private List<Result> HandleIntEditQuery(Query query) {
             List<Result> results = new List<Result>() {
-                new TyperResult() {
-                    Title = $"Test capitalise Rate enter value, current is {_config.CapitalizeRate}",
-                    SubTitle = query.Search,
-                    Action = (ActionContext _) => {
-                        try {
-                            if (query.SearchTerms.Length == 0) {
-                                ResetQuery(query);
-                                state = FlowTyperState.SETTINGS;
-                                return false;
-                            }
-
-                            string s = query.Search;
-                            int val = int.Parse(s);
-                            Func<int, bool> callback = intFieldHandlers[currIntField];
-                            bool success = callback.Invoke(val);
-                            ResetQuery(query);
-                            state = FlowTyperState.SETTINGS;
-                        }
-                        catch (Exception) {
-                            
-                        }
-
-                        return false;
-                    }
-                }
+                // TODO: This function is not currently used by any setting
+                // See the implementation for float field
             };
             return results;
         }
--- Flow.Launcher.Plugin.FlowTyper/Typer/TypingConfig.cs
+++ Flow.Launcher.Plugin.FlowTyper/Typer/TypingConfig.cs
@@ -6,18 +6,9 @@ namespace Flow.Launcher.Plugin.FlowTyper.Typer {
     public class TypingConfig {
         private bool _showIncorrectCharacters = true;
         private string _language = "english";
-        #region Punctuation
         private float _punctuationRate = 0f;
-        #endregion
-
-        #region Numbers
         private float _numbersRate = 0f;
-        #endregion
-
-        #region Capitalise
         private float _capitalizeRate = 0f;
-        #endregion
-
         public bool ShowIncorrectCharacters {
             get { return _showIncorrectCharacters; }
             set { _showIncorrectCharacters = value; }
@@ -26,28 +17,17 @@ namespace Flow.Launcher.Plugin.FlowTyper.Typer {
             get { return _language; }
             set { _language = value; }
         }
-
-        #region Punctuation Getter/Setter
-
         public float PunctuationRate {
             get { return _punctuationRate; }
             set { _punctuationRate = value; }
         }
-        #endregion
-
-        #region Numbers Getter/Setter
         public float NumbersRate {
             get { return _numbersRate; }
             set { _numbersRate = value; }
         }
-        #endregion
-
-        #region Capitalise Getter/Setter
-
         public float CapitalizeRate {
             get { return _capitalizeRate; }
             set { _capitalizeRate = value; }
         }
-        #endregion
     }
 }
